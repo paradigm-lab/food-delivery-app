1ecf1968f8f56cbcaa82114abfa26c16418a7a22
collins <collinsboniface1000@gmail.com>
2022-01-10 15:53:00 +0200

    Created food class with the fields and the contructor of the class and used the named parameter for the hightlight in the constructor

---
+++ lib/models/food.dart
@@ -0,0 +1,16 @@
+class Food {
+  String imgUrl;
+  String desc;
+  String name;
+  String waitTime;
+  num score;
+  String cal;
+  num price;
+  num quantity;
+  List<Map<String, String>> ingredients;
+  String about;
+  bool hightLight;
+
+  Food(this.imgUrl, this.desc, this.name, this.waitTime, this.score, this.cal,
+      this.price, this.quantity, this.ingredients, this.about, {this.hightLight = false});
+}
