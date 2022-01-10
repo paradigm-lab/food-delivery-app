c9a01ad69b94fb3facfd230c7bfbf3ffde46cd27
collins <collinsboniface1000@gmail.com>
2022-01-10 15:36:26 +0200

    Created a restaurant class in the model directory and having a constructor and fields in the class

---
+++ lib/models/restaurant.dart
@@ -0,0 +1,12 @@
+class Restaurant {
+  String name;
+  String waitTime;
+  String distance;
+  String label;
+  String logoUrl;
+  String desc;
+  num score;
+  Map<String, List<Food>> menu;
+
+  Restaurant(this.name, this.waitTime, this.distance, this.label, this.logoUrl, this.desc, this.score, this.menu);
+}
