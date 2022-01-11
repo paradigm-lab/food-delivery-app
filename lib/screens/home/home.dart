2d7dc58b61f7b8ca59deb26a9181df55ddbe4435
collins <collinsboniface1000@gmail.com>
2022-01-11 18:35:16 +0200

    Added the Restaurantinfo class into the home screen that will return a Text Widget

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/constants/colors.dart';
+import 'package:food_delivery_app/screens/home/widget/restaurant_info.dart';
 import 'package:food_delivery_app/widgets/custom_app_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,7 +22,9 @@ class _HomePageState extends State<HomePage> {
           CustomAppBar(
             Icons.arrow_back_ios_outlined,
             Icons.search_outlined,
-          )
+          ),
+
+          RestaurantInfo(),
         ],
       ),
     );
