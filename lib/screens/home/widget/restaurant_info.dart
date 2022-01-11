d875649a6c95941a316dd30bdd65a2879bd5ac75
collins <collinsboniface1000@gmail.com>
2022-01-11 18:40:33 +0200

    Create an instance of the Restaurant class and use the generateRestaurant method in the class which is static

--- lib/screens/home/widget/restaurant_info.dart
+++ lib/screens/home/widget/restaurant_info.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/cupertino.dart';
+import 'package:food_delivery_app/models/restaurant.dart';
 
 class RestaurantInfo extends StatelessWidget {
+
+  final restaurant = Restaurant.generateRestaurant();
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Restaurant info'),
+      margin: const EdgeInsets.only(top: 40),
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(restaurant.name),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Text(restaurant.waitTime),
+                        Text(restaurant.distance),
+                        Text(restaurant.label),
+                      ],
+                    )
+                  ],
+
+                ),
+              ],
+            )
+        ],
+      )
     );
   }
 
