2a45027445b683e8de2a61bc51655fbfeb3abd33
collins <collinsboniface1000@gmail.com>
2022-01-11 22:22:32 +0200

    Added the ListView which is scrollable horizonatally

--- lib/screens/home/widget/food_list.dart
+++ lib/screens/home/widget/food_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/constants/colors.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
 
 class FoodList extends StatelessWidget {
@@ -11,8 +12,33 @@ class FoodList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final category = restaurant.menu.keys.toList();
+
       return Container(
-        child: Text('List'),
+        height: 100,
+        padding: const EdgeInsets.symmetric(vertical: 30),
+        child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            itemBuilder: (context, index) => GestureDetector(
+              onTap: () => callback(index),
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: selected == index ? kPrimaryColor : Colors.white,
+                ),
+                child: Text(
+                  category[index],
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            separatorBuilder: (_, index) => const SizedBox(width: 20),
+            itemCount: category.length
+        )
       );
   }
 
