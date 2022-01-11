a340619d22581ee816087f03968730d459ff5c5c
collins <collinsboniface1000@gmail.com>
2022-01-11 22:32:50 +0200

    Passed another argument in the Expanded class constructor

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -45,6 +45,7 @@ class _HomePageState extends State<HomePage> {
                     });
                   },
                 pageController,
+                restaurant
               ),
           )
         ],
