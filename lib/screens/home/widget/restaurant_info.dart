84d8d13c109b142c66bfbfd4a1108264649c34e5
collins <collinsboniface1000@gmail.com>
2022-01-11 18:48:52 +0200

    Wrapped out the text Widget into a contatiner

--- lib/screens/home/widget/restaurant_info.dart
+++ lib/screens/home/widget/restaurant_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
 
 class RestaurantInfo extends StatelessWidget {
@@ -27,7 +28,19 @@ class RestaurantInfo extends StatelessWidget {
                     const SizedBox(height: 10),
                     Row(
                       children: [
-                        Text(restaurant.waitTime),
+                        Container(
+                          padding: const EdgeInsets.all(5),
+
+                          decoration: BoxDecoration(
+                            color: Colors.grey.withOpacity(0.4),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+
+                          child: Text(restaurant.waitTime,
+                                style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+
                         Text(restaurant.distance),
                         Text(restaurant.label),
                       ],
