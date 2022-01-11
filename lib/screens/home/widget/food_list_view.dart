3e8e8dff12fa63a6466c8177cf3e412dd1afd18e
collins <collinsboniface1000@gmail.com>
2022-01-11 22:34:46 +0200

    Create a FoodListView Class that extends the statelesswidget with the fields and constructor and the build method

---
+++ lib/screens/home/widget/food_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/models/restaurant.dart';
+
+
+
+class FoodListView extends StatelessWidget {
+
+  final int selected;
+  final Function callback;
+  final PageController pageController;
+  final Restaurant restaurant;
+
+
+  FoodListView(this.selected, this.callback, this.pageController,
+      this.restaurant);
+
+  @override
+  Widget build(BuildContext context) {
+      return Container(
+        child: Text("List View"),
+      );
+  }
+
+}
